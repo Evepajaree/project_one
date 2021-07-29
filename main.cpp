cc18a93d806eb5b6e4032a66b2e7870b53277dc4
Evepajaree <47072684+Evepajaree@users.noreply.github.com>
2021-07-30 03:28:12 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+#include "opencv2\core\core.hpp"
+#include "opencv2\contrib\contrib.hpp"
+#include "opencv2\highgui\highgui.hpp"
+#include "opencv2\objectect\objectect.hpp"
+#include "opencv2\opencv.hpp"
+
